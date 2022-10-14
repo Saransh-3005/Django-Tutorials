f815043b445fd24daa7e38eb2f7114200690f9a6
Shivam singh <shivamsingh8496@gmail.com>
2022-10-14 22:39:55 +0530

    Made Web-site more Dynamic

--- Project/myapp/models.py
+++ Project/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Feature:
+    featurId: int
+    deatils: str
+    head: str
--- Project/myapp/views.py
+++ Project/myapp/views.py
@@ -1,9 +1,31 @@
 from itertools import count
 from django.shortcuts import render
 import django.http 
+from .models import Feature
 # Create your views here.
 def index (request):
-    return render(request , 'index.html')
+    feature1 = Feature()
+    feature1.featurId=1
+    feature1.head = 'Fast'
+    feature1.deatils= 'This is the feature of the This Website that It is very VERY Fast'
+
+    feature2 = Feature()
+    feature2.featurId=2
+    feature2.head = 'Reliable'
+    feature2.deatils= 'This is the feature of the This Website that It is very VERY Fast'
+
+    feature3 = Feature()
+    feature3.featurId=3
+    feature3.head = 'Safe'
+    feature3.deatils= 'This is the feature of the This Website that It is very VERY Fast'
+
+    feature4 = Feature()
+    feature4.featurId=4
+    feature4.head = 'Secure'
+    feature4.deatils= 'This is the feature of the This Website that It is very VERY Fast'
+
+    features=[feature1,feature2,feature3,feature4]
+    return render(request , 'index.html',{'features':features})
 
 
 def counter(request):
