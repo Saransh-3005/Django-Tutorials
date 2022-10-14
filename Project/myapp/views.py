0f9f42cb3e57fa3b7743c94ca6cd6c2725fac62a
Shivam singh <shivamsingh8496@gmail.com>
2022-10-14 21:59:28 +0530

    New webpage has been made

--- Project/myapp/views.py
+++ Project/myapp/views.py
@@ -2,15 +2,15 @@ from itertools import count
 from django.shortcuts import render
 import django.http 
 # Create your views here.
-# def index (request):
-#     return render(request , 'index.html')
+def index (request):
+    return render(request , 'index.html')
 
 
-# def counter(request):
-#      word=request.POST['text1']
-#      count=len(word.split())
-#      return render(request,'counter.html',{"word":count})
+def counter(request):
+     word=request.POST['text1']
+     count=len(word.split())
+     return render(request,'counter.html',{"word":count})
 
     
-def index(request):
-    return render(request, 'sh.html')
+# def index(request):
+#     return render(request, 'sh.html')
